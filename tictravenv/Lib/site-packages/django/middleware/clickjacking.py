459fd1085db13a74ccc7d555f56ccba59be3643e
Muhammad Dwiki Yudhistira <yudhistiradwiki17@gmail.com>
2022-06-01 11:12:26 +0700

    ke django 4..0.4

--- tictravenv/Lib/site-packages/django/middleware/clickjacking.py
+++ tictravenv/Lib/site-packages/django/middleware/clickjacking.py
@@ -21,25 +21,28 @@ class XFrameOptionsMiddleware(MiddlewareMixin):
     response from being loaded in a frame in any site, set X_FRAME_OPTIONS in
     your project's Django settings to 'DENY'.
     """
+
     def process_response(self, request, response):
         # Don't set it if it's already in the response
-        if response.get('X-Frame-Options') is not None:
+        if response.get("X-Frame-Options") is not None:
             return response
 
         # Don't set it if they used @xframe_options_exempt
-        if getattr(response, 'xframe_options_exempt', False):
+        if getattr(response, "xframe_options_exempt", False):
             return response
 
-        response['X-Frame-Options'] = self.get_xframe_options_value(request,
-                                                                    response)
+        response.headers["X-Frame-Options"] = self.get_xframe_options_value(
+            request,
+            response,
+        )
         return response
 
     def get_xframe_options_value(self, request, response):
         """
         Get the value to set for the X_FRAME_OPTIONS header. Use the value from
-        the X_FRAME_OPTIONS setting, or 'SAMEORIGIN' if not set.
+        the X_FRAME_OPTIONS setting, or 'DENY' if not set.
 
         This method can be overridden if needed, allowing it to vary based on
         the request or response.
         """
-        return getattr(settings, 'X_FRAME_OPTIONS', 'SAMEORIGIN').upper()
+        return getattr(settings, "X_FRAME_OPTIONS", "DENY").upper()
--- tictravenv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!D:\Data Campus\Akademik\Semester 6\Kampus merdeka\tictrav_django\tictravenv\Scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
