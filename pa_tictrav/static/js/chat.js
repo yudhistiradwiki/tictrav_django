8edc723422745df3e99e6ff487525fde4a57d2f0
StevenPinantyo <stevenchristpa2001@gmail.com>
2022-06-02 19:45:28 +0700

    revisi chatbot

--- pa_tictrav/static/js/chat.js
+++ pa_tictrav/static/js/chat.js
@@ -35,7 +35,7 @@ function getTime() {
 
 // Gets the first message
 function firstBotMessage() {
-    let firstMessage = "How's it going?"
+    let firstMessage = "Hallo apa kabar, selamat datang di TicTrav?"
     document.getElementById("botStarterMessage").innerHTML = '<p class="botText"><span>' + firstMessage + '</span></p>';
 
     let time = getTime();
